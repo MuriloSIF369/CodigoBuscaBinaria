452b15e27e4cd98b64e99a0a5aa43f0173803935
Admin <Admin@MURILOPC>
2023-05-27 07:04:39 -0300

    Adicionar arquivos de projeto.

---
+++ Questão2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main()
+    {
+        string[] nomes = { "Alface", "Caramelo", "Escova", "Mesa", "Motor", "Salada", "Tigre" };
+
+        Console.Write("Pesquisa de palavra: ");
+        string pesquisa = Console.ReadLine();
+
+        int resultado = BuscaBinaria(nomes, pesquisa);
+
+        if (resultado >= 0)
+        {
+            Console.WriteLine($"Palavra encontrada na posição {resultado + 1}.");
+        }
+        else
+        {
+            Console.WriteLine("A palavra não está na lista.");
+        }
+    }
+
+    static int BuscaBinaria(string[] nomes, string pesquisa)
+    {
+        int esquerda = 0;
+        int direita = nomes.Length - 1;
+
+        while (esquerda <= direita)
+        {
+            int meio = (esquerda + direita) / 2;
+            int comparador = pesquisa.CompareTo(nomes[meio]);
+
+            if (comparador == 0)
+            {
+                return meio;
+            }
+            else if (comparador < 0)
+            {
+                direita = meio - 1;
+            }
+            else
+            {
+                esquerda = meio + 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ Questão3/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Numerics;
+using System.Xml.Serialization;
+
+class Program
+{
+    static void Main()
+    {
+    #region Menu
+    menu:
+        Console.Write("1 - Inserir um número no vetor.\n" +
+                        "2 - Pesquisar um número por busca binária.\n" +
+                        "3 - Pesquisar um número por busca linear.\n" +
+                        "4 - Sair\n" +
+                        "Opção: "); int choice = int.Parse(Console.ReadLine());
+        #endregion
+        while (choice != 4)
+        {
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Número à adicionar: ");
+                    int numbertoinsert = int.Parse(Console.ReadLine());
+                    int[] vector = { 10, 20, 30, 40, 50, 60, 70, 80, 90, numbertoinsert };
+                    BubbleSortOrdanator(vector);
+
+                    Console.Write("Aperte uma tecla para continuar...");
+                    Console.ReadKey();
+                    goto menu;
+
+                case 2:
+                    Console.Write("Número à pesquisar: ");
+                    int numbertosearch1 = int.Parse(Console.ReadLine());
+                    int[] vector2 = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+                    int index = BinarySearch(vector2, numbertosearch1);
+
+                    if (index != -1)
+                    {
+                        Console.WriteLine("Elemento encontrado na posição " + index);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Elemento não encontrado.");
+                    }
+
+                    Console.Write("Aperte uma tecla para continuar...");
+                    Console.ReadKey();
+                    goto menu;
+                case 3:
+                    Console.Write("Número à pesquisar: ");
+                    int numbertosearch = int.Parse(Console.ReadLine());
+                    int[] vector1 = { 20, 10, 30, 90, 50, 80, 10, 40, 60 };
+
+                    BubbleSortOrdanator(vector1);
+                    LinearSearch(numbertosearch, vector1);
+
+                    Console.Write("Aperte uma tecla para continuar...");
+                    Console.ReadKey();
+                    goto menu;
+
+            }
+        }
+
+    }
+    static void BubbleSortOrdanator(int[] vector)
+    {
+        int width = vector.Length;
+
+        for (int i = 0; i < width - 1; i++)
+        {
+            for (int j = 0; j < width - i - 1; j++)
+            {
+                if (vector[j] > vector[j + 1])
+                {
+                    int switcher = vector[j];
+                    vector[j] = vector[j + 1];
+                    vector[j + 1] = switcher;
+                }
+            }
+        }
+        Console.WriteLine("Vetor ordenado:");
+        for (int i = 0; i < width; i++)
+        {
+            Console.WriteLine(vector[i]);
+        }
+    }
+
+    static int LinearSearch(int target, int[] vector)
+    {
+        int counter = 0;
+        int width = vector.Length;
+        for (int i = 0; i < width; i++)
+        {
+            counter++;
+            if (vector[i] == target)
+            {
+                Console.WriteLine($"Número {target}, na posição {i}");
+                Console.WriteLine("Feitas {0} análises", counter);
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static int BinarySearch(int[] array, int target)
+    {
+        int left = 0;
+        int right = array.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target)
+            {
+                return mid;
+            }
+            else if (array[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
